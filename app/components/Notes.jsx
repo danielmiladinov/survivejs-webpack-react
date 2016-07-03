a57fd490c5681342d1a5340fb4d1279cbe61f3e8
Daniel Miladinov <daniel.miladinov@gmail.com>
2016-07-02 20:54:22 -0400

    Add Note drag and drop boilerplate
    Everything is in place, except the only drag-n-drop handler in place at
    the moment is a log message

--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import Editable from './Editable';
+import Note from './Note';
 
 export default ({notes, onValueClick, onEdit, onDelete}) => {
   return (
     <ul className="notes">{notes.map(note =>
       <li className="note" key={note.id}>
-        <Editable
-          editing={note.editing}
-          value={note.task}
-          onValueClick={onValueClick.bind(undefined, note.id)}
-          onEdit={onEdit.bind(undefined, note.id)}
-          onDelete={onDelete.bind(undefined, note.id)} />
+        <Note className="note" id={note.id} key={note.id}
+              onMove={({sourceId, targetId}) => console.log(`source: ${sourceId}, target: ${targetId}`)}>
+          <Editable
+            editing={note.editing}
+            value={note.task}
+            onValueClick={onValueClick.bind(undefined, note.id)}
+            onEdit={onEdit.bind(undefined, note.id)}
+            onDelete={onDelete.bind(undefined, note.id)} />
+        </Note>
       </li>)}
     </ul>
   );
