07d24bf8fdf745844804b8da99404a0a6cc5bf63
Daniel Miladinov <daniel.miladinov@gmail.com>
2016-07-05 16:35:57 -0400

    Prevent dragging a note while it's being edited

--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -7,7 +7,7 @@ export default ({notes, onValueClick, onEdit, onDelete}) => {
   return (
     <ul className="notes">{notes.map(note =>
       <li className="note" key={note.id}>
-        <Note className="note" id={note.id} key={note.id} onMove={LaneActions.move}>
+        <Note className="note" id={note.id} key={note.id} editing={note.editing} onMove={LaneActions.move}>
           <Editable
             editing={note.editing}
             value={note.task}
