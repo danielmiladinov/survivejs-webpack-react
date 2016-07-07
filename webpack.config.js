6b603000ee1a92062dce53bf6467de79c1ab4482
Daniel Miladinov <daniel.miladinov@gmail.com>
2016-07-06 21:41:55 -0400

    Add UglifyJsPlugin to lower the build output file size

--- webpack.config.js
+++ webpack.config.js
@@ -87,5 +87,12 @@ if (TARGET === 'start' || !TARGET) {
 }
 
 if (TARGET === 'build') {
-  module.exports = merge(common, {});
+  module.exports = merge(common, {
+    plugins: [
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false
+        }
+      })]
+  });
 }
